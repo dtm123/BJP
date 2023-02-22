e2fc528b2448d2147a2fb8dc7c9cf171952ce34c
Dan <danwest@gmail.com>
2023-02-22 10:54:02 -0800

    new file:   Ch1/StarFigures.java

---
+++ Ch1/StarFigures.java
@@ -0,0 +1,42 @@
+public class StarFigures {
+    public static void main(String[] args) {
+        rc();
+        br();
+        rc();
+        row();
+        br();
+        one();
+        rc();
+    }
+    
+    public static void row() {
+        System.out.println("*****");
+        System.out.println("*****");
+    }
+
+    public static void cross() {
+        System.out.println(" * * ");
+        mid();
+        System.out.println(" * * ");
+    }
+
+    public static void br() {
+        System.out.println();
+    }
+
+    public static void mid() {
+        System.out.println("  *");        
+    }
+
+    public static void rc() {
+        row();
+        cross();
+    }
+
+    public static void one() {
+        mid();
+        mid();
+        mid();
+    }
+
+}
