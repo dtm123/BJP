7a5e390bb7382c823b1928ddd1a867f4ee06b1bf
Dan <danwest@gmail.com>
2023-02-22 22:45:07 -0800

    new file:   Ch1/Shining.java

---
+++ Ch1/Shining.java
@@ -0,0 +1,46 @@
+public class Shining {
+    public static void main(String[] args) {
+        x6();
+    }
+    
+    public static void boy() {
+        System.out.println("All work and no play makes Jack a dull boy.");
+    }
+
+    public static void x2() {
+        boy();
+        boy();
+        boy();
+        boy();
+        boy();
+    }
+
+    public static void x3() {
+        x2();
+        x2();
+        x2();
+        x2();
+        x2();
+    }
+
+    public static void x4() {
+        x3();
+        x3();
+        x3();
+        x3();
+        x3();
+    }
+
+    public static void x5() {
+        x4();
+        x4();
+        x4();
+        x4();
+    }
+
+    public static void x6() {
+        x5();
+        x5();
+    }
+
+}
